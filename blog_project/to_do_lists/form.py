e0df993cbe221e120cf00b5206bbafccfdaa9112
Omid <omid@gmail.com>
2025-05-27 16:54:09 +0330

    added tasks in database

--- blog_project/to_do_lists/form.py
+++ blog_project/to_do_lists/form.py
@@ -1,5 +1,5 @@
 from django import forms
 class ToDoListForm(forms.Form):
     name = forms.CharField(max_length=250)
-    description = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea)
     status = forms.BooleanField(label='Complete?', required=False)
--- blog_project/to_do_lists/views.py
+++ blog_project/to_do_lists/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from .models import ToDoList
 from .form import ToDoListForm
+import datetime as dt
 # Create your views here.
 def todolist(request):
     tasks = ToDoList.objects.all()
@@ -10,8 +11,11 @@ def add(request):
     if request.method == 'POST':
         form = ToDoListForm(request.POST)
         if form.is_valid():
-            cd = form.cleaned_data
-            cd['name']
+            name = form.cleaned_data['name']
+            description = form.cleaned_data['description']
+            status = form.cleaned_data['status']
+            task = ToDoList(name = name, description = description,created = dt.datetime.now(), status = status)
+            task.save()
     else:
         form = ToDoList()
     return render (request,'to_do_lists/add.html',{'form':form})
